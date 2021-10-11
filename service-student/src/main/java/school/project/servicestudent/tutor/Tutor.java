eb671527f7fe9220bf7a0a28bd11fcf3e2654b2c
lenyn15 <you@example.com>
2021-10-11 16:11:14 -0500

    service-student entity tutor added

---
+++ service-student/src/main/java/school/project/servicestudent/tutor/Tutor.java
@@ -0,0 +1,86 @@
+package school.project.servicestudent.tutor;
+
+import lombok.*;
+import school.project.servicestudent.validation.Dni;
+import school.project.servicestudent.validation.Phone;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+
+@Entity
+@Table( name = "apoderado",
+        indexes = {
+                @Index( name = "index_id",
+                        columnList = "id" )
+        },
+        uniqueConstraints = {
+                @UniqueConstraint( name = "dni_unique",
+                                   columnNames = "dni" ),
+                @UniqueConstraint( name = "email_unique",
+                                   columnNames = "email" )
+        } )
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@Builder
+@ToString
+public class Tutor implements Serializable {
+
+    @Id
+    @GeneratedValue( strategy = GenerationType.AUTO )
+    @Column( nullable = false,
+             updatable = false )
+    private int id;
+
+    @NotEmpty( message = "Ingrese el nombre del apoderado" )
+    @Column( name = "nombres",
+             nullable = false,
+             length = 45 )
+    private String name;
+
+    @NotEmpty( message = "Ingrese los apelllidos del apoderado" )
+    @Column( name = "apellidos",
+             nullable = false,
+             length = 45 )
+    private String surname;
+
+    @NotEmpty( message = "Seleccione el sexo del apoderado" )
+    @Column( name = "sexo",
+             nullable = false,
+             length = 10 )
+    private String gender;
+
+    @Dni
+    @Size( min = 8,
+           max = 8,
+           message = "El tamaño del DNI es 8" )
+    @Column( nullable = false,
+             length = 8 )
+    private String dni;
+
+    @Phone
+    @NotEmpty( message = "Ingrese el telefono del apoderado" )
+    @Size( min = 9,
+           max = 9,
+           message = "El tamaño del telefono es 9" )
+    @Column( name = "telefono",
+             nullable = false,
+             length = 9 )
+    private String phone;
+
+    @Email( message = "El email ingresado, es invalido" )
+    @NotEmpty( message = "Ingrese el email del apoderado" )
+    @Column( nullable = false,
+             length = 65 )
+    private String email;
+
+    @NotEmpty( message = "Ingrese el trabajo del apoderado" )
+    @Column( name = "trabajo",
+             nullable = false,
+             length = 45 )
+    private String occupation;
+}
