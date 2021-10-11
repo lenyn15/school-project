1c884a9d0dccc6ad2dad3cd2135feec8ae629c58
lenyn15 <you@example.com>
2021-10-11 10:10:58 -0500

    service-student created

---
+++ service-student/src/main/java/school/project/servicestudent/ServiceStudentApplication.java
@@ -0,0 +1,13 @@
+package school.project.servicestudent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceStudentApplication {
+
+    public static void main( String[] args ) {
+        SpringApplication.run( ServiceStudentApplication.class, args );
+    }
+
+}
