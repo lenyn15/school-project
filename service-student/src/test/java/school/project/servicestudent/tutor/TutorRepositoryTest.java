c5adeadaf80c59067a59a4e0521fafd419f30b78
lenyn15 <you@example.com>
2021-10-13 17:27:10 -0500

    service-student tutor repository test success

---
+++ service-student/src/test/java/school/project/servicestudent/tutor/TutorRepositoryTest.java
@@ -0,0 +1,29 @@
+package school.project.servicestudent.tutor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class TutorRepositoryTest {
+
+    @Autowired
+    private TutorRepository underTest;
+
+    @Test
+    void itShouldFilterTutor() {
+        List<Tutor> list = underTest.filterTutor( "gmail" );
+        assertThat( list.size() ).isGreaterThan( 0 );
+    }
+
+    @Test
+    void itShouldFindByDni() {
+        Optional<Tutor> tutorDB = underTest.findByDni( "74224461" );
+        assertThat( tutorDB.isPresent() ).isTrue();
+    }
+}
