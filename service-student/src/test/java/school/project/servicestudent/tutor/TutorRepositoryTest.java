c2117864fac24b6393f1f67c93f16fba134a64c9
lenyn15 <you@example.com>
2021-10-15 14:12:11 -0500

    service-student tutors repository test modified

--- service-student/src/test/java/school/project/servicestudent/tutor/TutorRepositoryTest.java
+++ service-student/src/test/java/school/project/servicestudent/tutor/TutorRepositoryTest.java
@@ -1,35 +1,58 @@
 package school.project.servicestudent.tutor;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
+@DataJpaTest
 class TutorRepositoryTest {
 
     @Autowired
     private TutorRepository underTest;
 
-    @Test
-    void itShouldFilterTutor() {
-        List<Tutor> list = underTest.filterTutor( "gmail" );
-        assertThat( list.size() ).isGreaterThan( 0 );
+    @AfterEach
+    void tearDown() {
+        underTest.deleteAll();
     }
 
     @Test
     void itShouldCheckIfExistDni() {
-        Optional<Tutor> dni = underTest.existDni( "74224461" );
+        Tutor tutor = Tutor
+                .builder()
+                .name( "Lenyn Smith" )
+                .surname( "Goicochea" )
+                .gender( "Masculino" )
+                .dni( "74224460" )
+                .phone( "946025209" )
+                .email( "lenyn@gmail.com" )
+                .occupation( "Programador" )
+                .build();
+        underTest.save( tutor );
+
+        Optional<Tutor> dni = underTest.existDni( tutor.getDni() );
         assertThat( dni.isPresent() ).isTrue();
     }
 
     @Test
     void itShouldCheckIfExistEmail() {
-        Optional<Tutor> email = underTest.existEmail( "liz@gmail.com" );
+        Tutor tutor = Tutor
+                .builder()
+                .name( "Lenyn Smith" )
+                .surname( "Goicochea" )
+                .gender( "Masculino" )
+                .dni( "74224460" )
+                .phone( "946025209" )
+                .email( "lenyn@gmail.com" )
+                .occupation( "Programador" )
+                .build();
+        underTest.save( tutor );
+
+        Optional<Tutor> email = underTest.existEmail( tutor.getEmail() );
         assertThat( email.isPresent() ).isTrue();
     }
 }
