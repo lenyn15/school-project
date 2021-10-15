cf0f4623b99f3f07a41b8931241c5028af4e4cf8
lenyn15 <you@example.com>
2021-10-15 15:44:48 -0500

    service-student tutor repository test updated

--- service-student/src/test/java/school/project/servicestudent/tutor/TutorRepositoryTest.java
+++ service-student/src/test/java/school/project/servicestudent/tutor/TutorRepositoryTest.java
@@ -1,11 +1,8 @@
 package school.project.servicestudent.tutor;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-
-import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -14,11 +11,6 @@ class TutorRepositoryTest {
 
     @Autowired
     private TutorRepository underTest;
-
-    @AfterEach
-    void tearDown() {
-        underTest.deleteAll();
-    }
 
     @Test
     void itShouldCheckIfExistDni() {
@@ -34,8 +26,15 @@ class TutorRepositoryTest {
                 .build();
         underTest.save( tutor );
 
-        Optional<Tutor> dni = underTest.existDni( tutor.getDni() );
-        assertThat( dni.isPresent() ).isTrue();
+        Boolean expected = underTest.existDni( tutor.getDni() );
+        assertThat( expected ).isTrue();
+    }
+
+    @Test
+    void itShouldCheckIfDniDoesNotExist() {
+        String dni = "32740375";
+        Boolean expected = underTest.existDni( dni );
+        assertThat( expected ).isFalse();
     }
 
     @Test
@@ -52,7 +51,32 @@ class TutorRepositoryTest {
                 .build();
         underTest.save( tutor );
 
-        Optional<Tutor> email = underTest.existEmail( tutor.getEmail() );
-        assertThat( email.isPresent() ).isTrue();
+        Boolean expected = underTest.existEmail( tutor.getEmail() );
+        assertThat( expected ).isTrue();
+    }
+
+    @Test
+    void itShouldCheckIfEmailDoesNotExist() {
+        String email = "liz@gmail.com";
+        Boolean expected = underTest.existDni( email );
+        assertThat( expected ).isFalse();
+    }
+
+    @Test
+    void itShouldFound_WhenDniIsGiven() {
+        Tutor tutor = Tutor
+                .builder()
+                .name( "Lenyn Smith" )
+                .surname( "Goicochea" )
+                .gender( "Masculino" )
+                .dni( "74224460" )
+                .phone( "946025209" )
+                .email( "lenyn@gmail.com" )
+                .occupation( "Programador" )
+                .build();
+        underTest.save( tutor );
+
+        Tutor tutorDB = underTest.findByDni( tutor.getDni() );
+        assertThat( tutorDB ).isEqualTo( tutor );
     }
 }
