c707993f4ffe651c9b4d54868e3b9f2f79b6e857
lenyn15 <you@example.com>
2021-10-27 11:07:49 -0500

    service-student tutor controller updated

--- service-student/src/main/java/school/project/servicestudent/tutor/TutorController.java
+++ service-student/src/main/java/school/project/servicestudent/tutor/TutorController.java
@@ -5,12 +5,17 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import school.project.servicestudent.exception.ApiRequestException;
+import school.project.servicestudent.response.Response;
 
 import javax.validation.Valid;
+
 import java.util.List;
 
-import static org.springframework.http.HttpStatus.CREATED;
-import static org.springframework.http.HttpStatus.GONE;
+import static java.time.LocalDateTime.now;
+import static java.util.Map.of;
+import static org.springframework.http.HttpStatus.*;
+import static org.springframework.http.ResponseEntity.*;
+import static school.project.servicestudent.response.Response.*;
 import static school.project.servicestudent.validation.Message.formatMessage;
 
 @RestController
@@ -23,52 +28,85 @@ public class TutorController {
     private final TutorService tutorService;
 
     @GetMapping
-    public ResponseEntity<List<Tutor>> getAll() {
-        return ResponseEntity.ok( tutorService.showAll() );
+    public ResponseEntity<Response> getAll() {
+        return ok( builder().dateTime( now() )
+                            .data( of( "tutors", tutorService.showAll() ) )
+                            .message( "Todos los tutores cargados" )
+                            .status( OK )
+                            .statusCode( OK.value() )
+                            .build() );
     }
 
     @GetMapping( path = "/filter/{filter}" )
-    public ResponseEntity<List<Tutor>> search( @PathVariable( "filter" ) String word ) {
-        return ResponseEntity.ok( tutorService.searchTutors( word ) );
+    public ResponseEntity<Response> search( @PathVariable( "filter" ) String word ) {
+        List<Tutor> tutorList = tutorService.searchTutors( word );
+        return ok( builder().dateTime( now() )
+                            .data( of( "tutors", tutorList ) )
+                            .message( tutorList.isEmpty() ?
+                                      "No se pudo realizar la busqueda" :
+                                      "Tutores filtrados correctamente" )
+                            .status( OK )
+                            .statusCode( OK.value() )
+                            .build() );
     }
 
     @GetMapping( path = "/find/{dni}" )
-    public ResponseEntity<Tutor> filterByDni( @PathVariable( "dni" ) String dniTutor ) {
-        return ResponseEntity.ok( tutorService.searchByDni( dniTutor ) );
+    public ResponseEntity<Response> filterByDni( @PathVariable( "dni" ) String dniTutor ) {
+        return ok( builder().dateTime( now() )
+                            .data( of( "tutor", tutorService.searchByDni( dniTutor ) ) )
+                            .message( "Tutor encontrado" )
+                            .status( OK )
+                            .statusCode( OK.value() )
+                            .build() );
     }
 
     @GetMapping( path = "/{id}" )
-    public ResponseEntity<Tutor> getTutor( @PathVariable( "id" ) int idTutor ) {
-        return ResponseEntity.ok( tutorService.getOne( idTutor ) );
+    public ResponseEntity<Response> getTutor( @PathVariable( "id" ) int idTutor ) {
+        return ok( builder().dateTime( now() )
+                            .data( of( "tutor", tutorService.getOne( idTutor ) ) )
+                            .status( OK )
+                            .statusCode( OK.value() )
+                            .build() );
     }
 
     @PostMapping
-    public ResponseEntity<Tutor> addOne( @Valid @RequestBody Tutor tutor,
-                                         BindingResult result ) {
-        if ( result.hasErrors() ) {
-            throw new ApiRequestException( formatMessage( result ) );
-        }
-
-        return ResponseEntity
-                .status( CREATED )
-                .body( tutorService.add( tutor ) );
-    }
-
-    @PutMapping( path = "/{id}" )
-    public ResponseEntity<Tutor> updateOne( @PathVariable( "id" ) int idTutor,
-                                            @Valid @RequestBody Tutor tutor,
+    public ResponseEntity<Response> addOne( @Valid @RequestBody Tutor tutor,
                                             BindingResult result ) {
         if ( result.hasErrors() ) {
             throw new ApiRequestException( formatMessage( result ) );
         }
-        return ResponseEntity.ok( tutorService.update( idTutor, tutor ) );
+        return ok( builder().dateTime( now() )
+                            .data( of( "tutor", tutorService.add( tutor ) ) )
+                            .message( "Tutor creado correctamente" )
+                            .status( CREATED )
+                            .statusCode( CREATED.value() )
+                            .build() );
+    }
+
+    @PutMapping( path = "/{id}" )
+    public ResponseEntity<Response> updateOne( @PathVariable( "id" ) int idTutor,
+                                               @Valid @RequestBody Tutor tutor,
+                                               BindingResult result ) {
+        if ( result.hasErrors() ) {
+            throw new ApiRequestException( formatMessage( result ) );
+        }
+        return ok( builder().dateTime( now() )
+                            .data( of( "tutor", tutorService.update( idTutor, tutor ) ) )
+                            .message( "Tutor actualizado correctamente" )
+                            .status( OK )
+                            .statusCode( OK.value() )
+                            .build() );
     }
 
     @DeleteMapping( path = "/{id}" )
-    public ResponseEntity<?> deleteOne( @PathVariable( "id" ) int idTutor ) {
+    public ResponseEntity<Response> deleteOne( @PathVariable( "id" ) int idTutor ) {
         tutorService.destroy( idTutor );
-        return ResponseEntity
-                .status( GONE )
-                .body( null );
+        return ok( builder().dateTime( now() )
+                            .message( "Tutor con id "
+                                              + idTutor
+                                              + ", eliminado" )
+                            .status( GONE )
+                            .statusCode( GONE.value() )
+                            .build() );
     }
 }
