e1ae54c8a79c4b868668c926753fe15d18515ca5
lenyn15 <smithdash2570@gmail.com>
2021-11-28 16:04:06 -0500

    service-student tutor message errors indentations modified

--- service-student/src/main/java/school/project/servicestudent/validation/Message.java
+++ service-student/src/main/java/school/project/servicestudent/validation/Message.java
@@ -12,22 +12,20 @@ import java.util.stream.Collectors;
 public class Message {
 
     public static String formatMessage( BindingResult result ) {
-        List<Map<String, String>> errors = result
-                .getFieldErrors()
-                .stream()
-                .map( err -> {
-                    Map<String, String> error = new HashMap<>();
-                    error.put( err.getField(), err.getDefaultMessage() );
-                    return error;
+        List<Map<String, String>> errors = result.getFieldErrors()
+                                                 .stream()
+                                                 .map( err -> {
+                                                     Map<String, String> error = new HashMap<>();
+                                                     error.put( err.getField(), err.getDefaultMessage() );
+                                                     return error;
 
-                } )
-                .collect( Collectors.toList() );
+                                                 } )
+                                                 .collect( Collectors.toList() );
 
-        ErrorMessage errorMessage = ErrorMessage
-                .builder()
-                .code( "01" )
-                .messages( errors )
-                .build();
+        ErrorMessage errorMessage = ErrorMessage.builder()
+                                                .code( "01" )
+                                                .messages( errors )
+                                                .build();
 
         ObjectMapper mapper = new ObjectMapper();
         String jsonString = "";
