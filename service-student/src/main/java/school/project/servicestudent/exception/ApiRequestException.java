bad2ff6f267a4d76c1516bf55466b3173fa27267
lenyn15 <smithdash2570@gmail.com>
2021-11-28 16:06:03 -0500

    service-student custom exception - indentation modified

--- service-student/src/main/java/school/project/servicestudent/exception/ApiRequestException.java
+++ service-student/src/main/java/school/project/servicestudent/exception/ApiRequestException.java
@@ -1,6 +1,6 @@
 package school.project.servicestudent.exception;
 
-public class ApiRequestException extends RuntimeException{
+public class ApiRequestException extends RuntimeException {
 
     public ApiRequestException( String message ) {
         super( message );
