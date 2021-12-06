4aaded53e96b9f9a503981ae97a49add2531d849
lenyn15 <smithdash2570@gmail.com>
2021-12-06 11:45:23 -0500

    service-student eureka dependency enabled

--- service-student/src/main/java/school/project/servicestudent/ServiceStudentApplication.java
+++ service-student/src/main/java/school/project/servicestudent/ServiceStudentApplication.java
@@ -1,9 +1,11 @@
 package school.project.servicestudent;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 import static org.springframework.boot.SpringApplication.run;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class ServiceStudentApplication {
 
