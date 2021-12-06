9a90eb8218b78785f73fa9249c365454b3f68834
lenyn15 <smithdash2570@gmail.com>
2021-12-06 11:29:23 -0500

    registry service configuration added

--- registry-service/src/main/java/school/project/registryservice/RegistryServiceApplication.java
+++ registry-service/src/main/java/school/project/registryservice/RegistryServiceApplication.java
@@ -1,13 +1,16 @@
 package school.project.registryservice;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+import static org.springframework.boot.SpringApplication.run;
+
+@EnableEurekaServer
 @SpringBootApplication
 public class RegistryServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RegistryServiceApplication.class, args);
-	}
+    public static void main( String[] args ) {
+        run( RegistryServiceApplication.class, args );
+    }
 
 }
