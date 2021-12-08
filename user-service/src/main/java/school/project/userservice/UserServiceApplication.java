27f79ec2bfe86fac6214c8e1bf535dda9328cfcb
lenyn15 <smithdash2570@gmail.com>
2021-12-08 01:22:24 -0500

    user service created

---
+++ user-service/src/main/java/school/project/userservice/UserServiceApplication.java
@@ -0,0 +1,13 @@
+package school.project.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+}
